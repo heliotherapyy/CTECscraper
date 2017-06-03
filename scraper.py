fcac34205e2136441fed451fc9c5e6cd0c60e35c
lennyKim <heliotherapyy@gmail.com>
2017-06-03 04:10:40 -0500

    (add) make it to the end of this journey

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import csv
 
 chrome_path = r"/Users/Lenny/Downloads/chromedriver"
 driver = webdriver.Chrome(chrome_path)
@@ -14,8 +16,8 @@ username_path = driver.find_element_by_css_selector("input#IDToken1")
 password_path = driver.find_element_by_css_selector("input#IDToken2")
 login_path = driver.find_element_by_name("Login.Submit")
 
-username_path.send_keys("username")
-password_path.send_keys("password")
+username_path.send_keys("dsf")
+password_path.send_keys("asdf!!")
 login_path.submit()
 
 ####### Duo Mobile should be done manually
@@ -46,6 +48,7 @@ for option in select_form.find_elements_by_tag_name("option"):
 
 
 print("subjects: \n", subjects)
+subjects = subjects[1:]
 
 select_form = Select(driver.find_element_by_id("NW_CT_PB_SRCH_SUBJECT"))
 driver.implicitly_wait(1)
@@ -59,6 +62,45 @@ path.click()
 path = driver.find_element_by_name('NW_CT_PB_SRCH_SRCH_BTN')
 path.click()
 driver.implicitly_wait(1)
+
+# Get the table that shows all courses
+path = driver.find_element_by_xpath('//*[@id="NW_CT_PV_DRV$scroll$0"]/tbody')
+courses = path.find_elements_by_tag_name('tr')
+link = courses[1].find_element_by_tag_name('a')
+
+# Get the table that shows all quarters of selected course
+for course in courses[1:]:
+  link = course.find_element_by_tag_name('a')
+  link.click()
+  driver.implicitly_wait(2)
+  tables = driver.find_elements_by_class_name('PSLEVEL1GRIDNBO')
+  for quarter in tables[:-1]:
+    course_links = quarter.find_elements_by_tag_name('a')
+    courses_window = driver.current_window_handle
+    for link in course_links[3:]:
+      link.click()
+      handle = driver.window_handles[1]
+      driver.switch_to_window(handle) # now inside the CTEC review
+      driver.switch_to_frame("TargetContent")
+      prof_name = driver.find_element_by_css_selector('.PSEDITBOX_DISPONLY#NW_CT_PV_NAME_NAME')
+      prof_name = prof_name.text.encode('ascii', 'ignore')
+      path = driver.find_element_by_xpath('//*[@id="win16divNW_CT_PV3_DRV_DESCRLONG$0"]/div/p/font')
+      review = path.text.encode('ascii', 'ignore') # professor name & reviews selected
+      data = [prof_name, review]
+      with open('reviews.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
